b88b755c599e7da3a6aa452784db562bf069c186
Quoc <akkla002@gmail.com>
2019-04-18 07:56:09 +0700

    create first

---
+++ Server/DAO/DataProvider.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Server.DAO
+{
+    public class DataProvider
+    {
+        private static DataProvider instance = null;
+        private string connectionSTR = @"";
+
+        public static DataProvider Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new DataProvider();
+                return instance;
+            }
+            private set { instance = value; }
+        }
+
+        #region Methods
+        private SqlCommand GetCommand(string query, object[] parameter = null)
+        {
+            SqlCommand command = new SqlCommand(query);
+            if (parameter != null)
+            {
+                string[] listPara = query.Split(' ');
+                int i = 0;
+                foreach (string item in listPara)
+                {
+                    if (item.Contains('@'))
+                    {
+                        command.Parameters.AddWithValue(item, parameter[i]);
+                        i++;
+                    }
+                }
+            }
+            return command;
+        }
+        public DataTable ExecuteQuery(string query, object[] parameter = null)
+        {
+            DataTable data = new DataTable();
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = GetCommand(query, parameter);
+                command.Connection = connection;
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                adapter.Fill(data);
+                connection.Close();
+            }
+            return data;
+        }
+        public int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+            int result = 0;
+            using (SqlConnection connection = new SqlConnection(connectionSTR))
+            {
+                connection.Open();
+                SqlCommand command = GetCommand(query, parameter);
+                command.Connection = connection;
+                result = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return result;
+        }
+
+
+
+
+        #endregion
+    }
+}
